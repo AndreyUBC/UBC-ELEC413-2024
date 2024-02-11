a225ad73cc21886f8052db2c9c43a258214c6cca
AndreyUBC <159112888+AndreyUBC@users.noreply.github.com>
2024-02-10 18:35:25 -0800

    Update design_Andrey.py

--- Layout/Designs/design_Andrey.py
+++ Layout/Designs/design_Andrey.py
@@ -43,12 +43,26 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     if not cell_bragg:
         raise Exception ('Cannot load Bragg grating cell; please check the script carefully.')
 
+    waveguide_type_mm = 'Multimode Strip TE 1310 nm, w=2000 nm'
+
+    # Lets start with connection
+    height = 20000
+    width = 200000
+
     # instantiate y-branch (attached to input waveguide)
     inst_y1 = connect_cell(inst_wg1, 'opt2', cell_y, 'opt2')
+    
+    # instantiate Bragg grating (attached to y branch)
+    inst_bragg1 = connect_cell(inst_y1, 'opt', cell_bragg, 'opt1')
 
     # instantiate taper from 350 nm waveguide y-branch to 385 nm Bragg grating
-    inst_taper1 = connect_cell(inst_y1, 'opt1', cell_taper, 'opt')
+    inst_taper1 = connect_cell(inst_bragg1, 'opt2', cell_taper, 'opt')
     
+    inst_taper2 = connect_cell(inst_taper1, 'opt2', cell_taper, 'opt2')
+    inst_taper2.transform(Trans(width,0))
+    
+
+
     # instantiate Bragg grating (attached to y branch)
     inst_bragg1 = connect_cell(inst_taper1, 'opt2', cell_bragg, 'opt1')
 
@@ -79,7 +93,7 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     '''
     # connect_pins_with_waveguide(inst_y1, 'opt3', inst_wg3, 'opt1', waveguide_type=waveguide_type)
     
-    '''
+    
 
     # Good, outer Waveguide
     try:
@@ -91,6 +105,7 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
             waveguide_type='Strip TE 1310 nm, w=385 nm (core-clad)', 
             turtle_A = [10,-90,215,-90,350,-90] )
 
+    '''
     # Good, Inner Waveguide
     try:
         connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
