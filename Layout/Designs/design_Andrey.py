aa5bc98c4e8dcac43fab306e3382053734c7335b
AndreyUBC <159112888+AndreyUBC@users.noreply.github.com>
2024-02-10 20:35:36 -0800

    Update design_Andrey.py
    Design for project 2

--- Layout/Designs/design_Andrey.py
+++ Layout/Designs/design_Andrey.py
@@ -8,18 +8,9 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     ly = cell.layout()
     library = ly.technology().name
 
+    # took it from Lucas
     cell_taper = ly.create_cell('ebeam_taper_350nm_2000nm_te1310', library)
 
-    #####
-    # designer circuit:
-
-    # Create a physical text label so we can see under the microscope
-    # How do we find out the PCell parameter variables?
-    '''
-    c = ly.create_cell('TEXT','Basic')
-    [p.name for p in c.pcell_declaration().get_parameters() if c.is_pcell_variant]
-    c.delete()
-    '''
     # returns: ['text', 'font_name', 'layer', 'mag', 'inverse', 'bias', 'cspacing', 'lspacing', 'eff_cw', 'eff_ch', 'eff_lw', 'eff_dr', 'font']
     from SiEPIC.utils import get_technology_by_name
     TECHNOLOGY = get_technology_by_name(library)
@@ -33,7 +24,7 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     cell.insert(CellInstArray(cell_text.cell_index(), Trans(Trans.R0, 25000,125000)))                
 
     # load the cells from the PDK
-    # choose appropriate parameters
+    # my parameters
     cell_bragg = ly.create_cell('ebeam_pcell_bragg_grating', library, {
         'number_of_periods':10,
         'grating_period': 0.272,
@@ -43,9 +34,10 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     if not cell_bragg:
         raise Exception ('Cannot load Bragg grating cell; please check the script carefully.')
 
+    # Define a waveguide to connect the big side of the tapers
     waveguide_type_mm = 'Multimode Strip TE 1310 nm, w=2000 nm'
 
-    # Lets start with connection
+    # Define the parameters of the move
     height = 20000
     width = 200000
 
@@ -55,12 +47,12 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     # instantiate Bragg grating (attached to y branch)
     inst_bragg1 = connect_cell(inst_y1, 'opt1', cell_bragg, 'opt1')
 
-    # instantiate taper from 350 nm waveguide y-branch to 385 nm Bragg grating
+    ########## Initiate all the tapers and wveguides ########### 
     inst_taper1 = connect_cell(inst_bragg1, 'opt2', cell_taper, 'opt')
     
     inst_taper2 = connect_cell(inst_taper1, 'opt2', cell_taper, 'opt2')
     inst_taper2.transform(Trans(width,0))
-    # Connects t1 and t2
+    
     connect_pins_with_waveguide(inst_taper1, 'opt2', inst_taper2, 'opt2', waveguide_type=waveguide_type_mm)
 
     inst_taper3 = connect_cell(inst_taper1, 'opt2', cell_taper, 'opt2')
@@ -77,10 +69,10 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     inst_taper5.transform(Trans(-width,height))
 
     connect_pins_with_waveguide(inst_taper4, 'opt', inst_taper5, 'opt', waveguide_type=waveguide_type)
-    #############################Fist Line
+    
     inst_taper6 = connect_cell(inst_taper5, 'opt2', cell_taper, 'opt2')
     inst_taper6.transform(Trans(width,0))
-    # Connects t1 and t2
+
     connect_pins_with_waveguide(inst_taper5, 'opt2', inst_taper6, 'opt2', waveguide_type=waveguide_type_mm)
 
     inst_taper7 = connect_cell(inst_taper5, 'opt2', cell_taper, 'opt2')
@@ -166,23 +158,10 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     inst_bragg2 = connect_cell(inst_taper22, 'opt', cell_bragg, 'opt2')
 
     connect_pins_with_waveguide(inst_bragg2, 'opt1', inst_wg2, 'opt1', waveguide_type=waveguide_type, turtle_A = [20, -90, 132.7, 90], error_min_bend_radius=False)
-    
-    
-    '''
-    make a long waveguide, back and forth, 
-    target 0.2 nm FSR assuming ng = 4
-    > wavelength=1270e-9; ng=4; fsr=0.2e-9;
-    > L = wavelength**2/2/ng/fsr
-    > L * 1e6
-    > 1000 [microns]
-    using "turtle" routing
-    https://github.com/SiEPIC/SiEPIC-Tools/wiki/Scripted-Layout#adding-a-waveguide-between-components
-    '''
-    # connect_pins_with_waveguide(inst_y1, 'opt3', inst_wg3, 'opt1', waveguide_type=waveguide_type)
-    
-    
+    ########## Finish initiateing all the tapers and wveguides ########### 
 
-    # Good, outer Waveguide
+
+    # Outer WaveGuide Connection 
     try:
         connect_pins_with_waveguide(inst_y1, 'opt3', inst_wg3, 'opt1', 
             waveguide_type='Strip TE 1310 nm, w=350 nm (core-clad)', 
@@ -192,17 +171,4 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
             waveguide_type='Strip TE 1310 nm, w=385 nm (core-clad)', 
             turtle_A = [10,-90,215,-90,355,-90] )
 
-    '''
-    # Good, Inner Waveguide
-    try:
-        connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
-            waveguide_type='Strip TE 1310 nm, w=385 nm (core-clad)', 
-            turtle_A = [325,90,20,90,345,-90,20,-90,300,90,20,90,300,-90,20,-90,300,90,20,90,300,-90,20,-90,300,90,20,90,300,-90,20,-90,300,90,20,90,300,-90,20,-90] )
-    except:    
-        connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
-            waveguide_type='Strip TE 1310 nm, w=350 nm (core-clad)', 
-            turtle_A = [325,90,20,90,345,-90,20,-90,300,90,20,90,300,-90,20,-90,300,90,20,90,300,-90,20,-90,300,90,20,90,300,-90,20,-90,300,90,20,90,300,-90,20,-90] )
-
-            
-    '''
     return inst_wg1, inst_wg2, inst_wg3
