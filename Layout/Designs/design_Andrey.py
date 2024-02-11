7e002e86a7b57564137a6c869e697f0fbbc54cd8
AndreyUBC <159112888+AndreyUBC@users.noreply.github.com>
2024-02-10 16:51:02 -0800

    Update design_Andrey.py

--- Layout/Designs/design_Andrey.py
+++ Layout/Designs/design_Andrey.py
@@ -1,7 +1,7 @@
 from pya import *
 
  
-def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
+def design_student1(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     
     # load functions
     from SiEPIC.scripts import connect_pins_with_waveguide, connect_cell
@@ -33,15 +33,20 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     # load the cells from the PDK
     # choose appropriate parameters
     cell_bragg = ly.create_cell('ebeam_pcell_bragg_grating', library, {
-        'number_of_periods':10,
-        'grating_period': 0.272,
-        'corrugation_width': 0.05,
-        'wg_width': 0.350,
-        'sinusoidal': False})
+        'number_of_periods':60,
+        'grating_period': 0.270,
+        'corrugation_width': 0.08,
+        'wg_width': 0.385,
+        'sinusoidal': True})
     if not cell_bragg:
         raise Exception ('Cannot load Bragg grating cell; please check the script carefully.')
 
-    cell_taper = ly.create_cell('ebeam_taper_350nm_2000nm_te1310', library)
+    cell_taper = ly.create_cell('ebeam_pcell_taper', library, {
+        'wg_width1': 0.350,
+        'wg_width2': 0.385,
+            })
+    if not cell_taper:
+        raise Exception ('Cannot load taper cell; please check the script carefully.')
 
     # instantiate y-branch (attached to input waveguide)
     inst_y1 = connect_cell(inst_wg1, 'opt2', cell_y, 'opt2')
@@ -77,9 +82,6 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
     using "turtle" routing
     https://github.com/SiEPIC/SiEPIC-Tools/wiki/Scripted-Layout#adding-a-waveguide-between-components
     '''
-    connect_pins_with_waveguide(inst_y1, 'opt3', inst_wg3, 'opt1', waveguide_type=waveguide_type)
-    connect_pins_with_waveguide(inst_bragg2, 'opt1', inst_wg2, 'opt1', waveguide_type=waveguide_type, error_min_bend_radius=False)
-    '''
     try:
         connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
             waveguide_type='Strip TE 1310 nm, w=385 nm (core-clad)', 
@@ -88,7 +90,5 @@ def design_Andrey(cell, cell_y, inst_wg1, inst_wg2, inst_wg3, waveguide_type):
         connect_pins_with_waveguide(inst_bragg1, 'opt2', inst_bragg2, 'opt2', 
             waveguide_type='Strip TE 1310 nm, w=350 nm (core-clad)', 
             turtle_A = [250,90,20,90,250,-90,20,-90,250,90,20,90,250,-90,20,-90] )
-    '''
-    return inst_wg1, inst_wg2, inst_wg3
-# Enter your Python code here
 
+    return inst_wg1, inst_wg2, inst_wg3
